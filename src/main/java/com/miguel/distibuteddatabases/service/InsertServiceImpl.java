cce63e75fae85da6ced47353f661bb3971fb2758
Miguel Fernandez <fernandez.s.miguel96@gmail.com>
2019-05-07 16:40:40 +0200

    Implementacion del service

--- src/main/java/com/miguel/distibuteddatabases/service/InsertServiceImpl.java
+++ src/main/java/com/miguel/distibuteddatabases/service/InsertServiceImpl.java
@@ -2,31 +2,40 @@ package com.miguel.distibuteddatabases.service;
 
 import com.miguel.distibuteddatabases.model.Direccion;
 import com.miguel.distibuteddatabases.model.Persona;
+import com.miguel.distibuteddatabases.repository.DireccionDao;
 import com.miguel.distibuteddatabases.repository.PersonaDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@Transactional
 public class InsertServiceImpl implements InsertService {
 
     @Autowired
     private PersonaDao personaDao;
 
     @Autowired
-    private Direccion dirDao;
+    private DireccionDao dirDao;
 
     @Override
     public void save(Persona pers, Direccion dir) {
-
+        personaDao.save(pers);
+        dirDao.save(dir);
     }
 
     @Override
     public List<Persona> mostrarPersona() {
-        return null;
+        List<Persona> x = new ArrayList(personaDao.findAll());
+        return x;
     }
 
     @Override
     public List<Direccion> mostrarDireccion() {
-        return null;
+        List<Direccion> x = new ArrayList(dirDao.findAll());
+        return x;
     }
 }
